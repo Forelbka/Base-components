10e4783bc9dd41db003ac20d4e43333e1da7dd01
Forelbka <ivan-8180@yandex.ru>
2022-10-05 18:41:53 +0500

    new file:   zip_list.py

---
+++ zip_list.py
@@ -0,0 +1,40 @@
+class ziplist:
+    def __init__(self, lis):
+        self.lis = lis
+        self.zipedlis = []
+        self.repeats = []
+        self.compress()
+    def __add__(self, other):
+        if other == ziplist:
+            return ziplist(self.uncompress() + other.uncompress()).compress()
+        elif other == list:
+            return self.uncompress() + other
+        else:
+            return self.uncompress() + list(other)
+    def __str__(self):
+        return str(self.uncompress())
+    def __repr__(self):
+        return f'Compresse: {bool(self.zipedlis)}, Compressed List: {self.zipedlis}, Uncompressed List: {self.lis}'
+    def compress(self):
+        for el in set(self.lis):
+            if self.lis.count(el) > 1:
+                self.repeats.append(el)
+        print(self.repeats)
+        for el in self.lis:
+            if el in self.repeats:
+                self.zipedlis.append(str(self.repeats.index(el)))
+            else:
+                self.zipedlis.append(el)
+        self.lis = []
+    def uncompress(self):
+        for el in self.zipedlis:
+            if type(el) == str:
+                self.lis.append(self.repeats[int(el)])
+            else:
+                self.lis.append(el)
+        return [self.lis.pop(0) for _ in range(len(self.lis))]
+    
+a = ziplist([1, 2, 5, 4, 7, 1, 1, 2, 2, 7, 7, 7])
+print(a)
+print(repr(a))
+print(a.uncompress())
