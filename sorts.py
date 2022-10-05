9cf966115a65ec79e74581bf392847268d5c1cdf
Forelbka <ivan-8180@yandex.ru>
2022-10-05 16:18:11 +0500

    modified:   sorts.py

--- sorts.py
+++ sorts.py
@@ -48,22 +48,15 @@ def insertsort(lis):
 
 def fastsort(lis):
     if len(lis) < 2:
-        print(lis)
         return(lis)
-    ref_num = len(lis) // 2
-    for el in lis[:ref_num] + lis[ref_num:]:
-        if el >= lis[ref_num] and lis.index(el) < ref_num:
-            lis.append(lis.pop(lis.index(el)))
-            ref_num -= 1
-        elif lis.index(el) > ref_num:
-            lis.insert(-1, lis.pop(lis.index(el)))
-            ref_num += 1
-        if ref_num == len(lis) - 1 or ref_num == 0:
-            break
-    if len(lis) == 2:
-        print(lis)
-        return lis
-    print(lis)
-    return fastsort(lis[:ref_num]) + [lis[ref_num]] + fastsort(lis[ref_num + 1:])
+    ref_el = lis.pop(len(lis) // 2)
+    new_lis = []
+    for i in range(len(lis)):
+        if lis[i] < ref_el:
+            new_lis.append(lis[i])
+    lis = [el for el in lis if not el in new_lis]
+    return fastsort(new_lis) + [ref_el] + fastsort(lis)
+    
+
 
 print(fastsort([1, 2, -10, 7, 0, 100, 10]))
