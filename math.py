42c32c42dd0ee016139f9fe7ccdf931a5125631c
Forelbka <ivan-8180@yandex.ru>
2022-10-17 21:01:48 +0500

    modified:   math.py 	modified:   tests.ipynb

--- math.py
+++ math.py
@@ -1,4 +1,4 @@
-from symbol import del_stmt
+from re import S
 import tkinter as tk
 from turtle import *
 
@@ -13,6 +13,24 @@ entr.pack()
 label.pack()
 button.pack()
 
+def get_f(strin, x):
+    strin = strin[:]
+    l_rez = 1
+    if 'x' in strin:
+        if not '^' in strin:
+            l_rez *= x
+        strin = strin.split('x')
+        if strin[0]:
+            if strin[0] == '-':
+                l_rez *= -1
+            else:
+                l_rez *= float(strin[0])
+        if strin[1]:
+            l_rez *= x ** float(strin[1][1:])
+    else:
+        l_rez *= float(strin)
+    return l_rez
+
 class Function():
     def __init__(self, string):
         self.string = string
@@ -22,40 +40,54 @@ class Function():
         self.c = 0
 
     def get_func(self, x):
-        strin = self.string.split()
-        gl_rez = 0
-        l_rez = 1
-        znak = True
-        for el in strin:
-            l_rez = 1
-            if len(el) > 1 or el == 'x' or el in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
-                if not znak:
-                    l_rez *= -1
-                    znak = True
-                if 'x' in el:
-                    if not '^' in el:
-                        l_rez *= x
-                    el = el.split('x')
-                    if el[0]:
-                        if el[0] == '-':
-                            l_rez *= -1
+        strin = self.string
+        znak = 1
+        summ = 0
+        l_sum = 1
+        if '(' in strin or ')' in strin:
+                    count_s = 0
+                    ind_f = 0
+                    if strin[0] != '(':
+                        l_sum *= get_f(strin[:strin.find('(')], x)
+                        ind_f = strin.find('(')
+                    for n, zn in enumerate(strin[ind_f:]):
+                        if zn == '(':
+                            count_s += 1
+                        elif zn == ')':
+                            count_s -= 1
+                        if count_s == 0:
+                            ferst_f = Function(strin[ind_f + 1:n])
+                            sec_f = Function(strin[n + 1:])
+                            if n != len(strin) and strin[n + 1:]:
+                                summ += ferst_f.get_func(x) + sec_f.get_func(x) * l_sum
+                            else:
+                                summ += ferst_f.get_func(x)
+                            break
+        else:
+            strin = strin.split()
+            for el in strin:
+                if len(el) > 1 or el == 'x' or el in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
+                    if '/' in el:
+                        n = el.find('/')
+                        if get_f(el[n + 1:], x) == 0:
+                            return None
                         else:
-                            l_rez *= float(el[0])
-                    if el[1]:
-                        l_rez *= x ** float(el[1][1:])
+                            summ += (get_f(el[:n], x) / get_f(el[n + 1:], x)) * znak
+                            znak = 1
+                    else:
+                        summ += get_f(el, x) * znak
+                        znak = 1
                 else:
-                    l_rez *= float(el)
-                gl_rez += l_rez
-            else:
-                if el == '-':
-                    znak = False
-        return gl_rez
+                    if el == '-':
+                        znak = -1
+        return summ
 
 
     def print_func(self, start=(y * -1), finish=y):
         if '^0.' in self.string and start < 0:
             start = 0
         clear()
+        home()
         speed(10000)
         pensize(2)
         step = (y * 2) // 10
@@ -69,8 +101,11 @@ class Function():
             goto(xcor(), -10)
             goto(xcor(), 0)
         goto(y, 0)
+        stamp()
         goto(0, 0)
         goto(0, y)
+        lt(90)
+        stamp()
         goto(0, y * -1)
         for _ in range(9):
             goto(0, ycor() + step)
@@ -82,8 +117,13 @@ class Function():
         results = [self.get_func(i / step) for i in range(start, finish)]
         goto(start, results[0] * step)
         pd()
-        for i in range(start, finish):
-            goto(i, int(results[i - start] * step))
+        for i in range(start, finish - 1):
+            if results[i - start]:
+                goto(i, int(results[i - start] * step))
+            else:
+                pu()
+                goto(i + 1, int(results[i - start + 1] * step))
+                pd()
 
 
 def handle_keypress(event):
@@ -92,4 +132,8 @@ def handle_keypress(event):
 
 
 button.bind("<Button-1>", handle_keypress)
+
+fu = Function('x + 1')
+print(fu.get_func(10))
+
 mainloop()
