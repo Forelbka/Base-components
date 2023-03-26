45fcabae476395134c4c82ace95fe72537e53407
Forelbka <ivan-8180@yandex.ru>
2023-03-26 11:09:38 +0500

    new file:   math_1.py 	modified:   matrixs.py 	modified:   test_math.py

--- matrixs.py
+++ matrixs.py
@@ -9,6 +9,8 @@ class matrix():
         matr = self.matr
         if len(matr) != len(matr[0]):
             return 'Error'
+        if len(matr) == 1:
+            return matr[0][0]
         if len(matr) == 2:
             return matr[0][0] * matr[1][1] - matr[0][1] * matr[1][0]
         det = 0
@@ -28,6 +30,28 @@ class matrix():
             new_matrix = matrix(new_matrix).transpon()
             ret.append(matrix(new_matrix).determinant() // det)
         return ' '.join(map(str, ret))
+ 
+    def revers_matrix(self):
+        matr = self.matr
+        minor_matr = []
+        if len(matr) != len(matr[0]):
+            return 'Error'
+        for i in range(len(matr)):
+            minor_matr.append([])
+            for j in range(len(matr[-1])):
+                minor_matr[-1].append(matrix([el[:j] + el[j + 1:] for n, el in enumerate(matr) if n != i]).determinant() * ((-1) ** i)* ((-1) ** j) * (-1))
+        return matrix(minor_matr).transpon()
+    
+    def set_zero(self):
+        self.matr = [[0 for i in range(len(self.matr[0]))] for _ in range(len(self.matr))]
+    
+    def set_singl(self):
+        if len(self.matr) != len(self.matr[0]):
+            return 'Error'
+        self.matr = [[0 for i in range(len(self.matr[0]))] for _ in range(len(self.matr))]
+        for i in range(min((len(self.matr), len(self.matr[0])))):
+            self.matr[i][i] = 1
+ 
     def __add__(self, other):
         m1 = self.matr
         m2 = other.matr
@@ -59,18 +83,6 @@ class matrix():
         else:
             return 'Error 022'
     
-    def __truediv__(self, other):
-        return 'Error 023'
-    
-    def __floordiv__(self, orher):
-        return 'Error 024'
-    
-    def __mod__(self, other):
-        return 'Error 025'
-    
-    def __divmod__(self, other):
-        return 'Error 026'
-    
     def __lt__(self, other):
         return len(self.matr) * len(self.matr[0]) < len(other.matr) * len(other.matr[0])
     
@@ -98,16 +110,17 @@ class matrix():
     
     def __setitem__(self, key, el):
         self.matr[key] = el
-
+ 
 tes1 = matrix([
     [3, -3, -5, 8],
     [-3, -5, -7, 5],
     [2, -5, -7, 7],
     [-4, 3, 5, -6]
 ])
-
+ 
 tes2 = matrix([list(map(int, input().split())) for _ in range(int(input()))])
 
 
-
-print(tes2.determinant())
+ 
+print(tes1.set_singl())
+print(tes1)
